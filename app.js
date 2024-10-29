77c22503a93fe21137f147b2548ea3f6ee5c3d5c
Jacob Wingate <jacob.wingate33@gmail.com>
2024-10-29 11:52:32 +0100

    add finished ledger request for last index

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const xrpl = require('xrpl')
 
 const displayLastLedgerIndex = async () => {
-  console.log(`Hello world from the XRP ledger, the last index was`)
+  const client = new xrpl.Client('wss://xrplcluster.com') // holds the connection to the ledger
+  await client.connect() // connect to the ledger
+
+  const ledgerInfo = await client.request({
+    command: 'ledger',
+    ledger_index: 'validated',
+  })
+
+  console.log(
+    `Hello world from the XRP ledger, the last index was: ${ledgerInfo.result.ledger_index}`
+  )
+
+  await client.disconnect() // close down coneection when finished
 }
 
 displayLastLedgerIndex()
