23c7b4625b815de760b20a0535e5685a18750c44
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-09 21:57:53 +0100

    remove buggy code

--- AccountDelete/account-delete.js
+++ AccountDelete/account-delete.js
@@ -2,15 +2,3 @@ import { Client, Wallet } from 'xrpl'
 
 // Get wallet from seed
 const wallet = Wallet.fromSeed('TBC')
-
-// New ledger connection
-const client = new Client('TBC')
-await client.connect()
-
-try {
-  // Create account delete tx object
-
-  const deleteTx = {
-    
-  }
-}
