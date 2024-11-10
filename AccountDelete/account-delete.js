f1f0e29dfaa72931a6df76a2ca9464045e839ac6
Jacob Wingate <jacob.wingate33@gmail.com>
2024-11-10 20:59:46 +0100

    re add lendger connection

--- AccountDelete/account-delete.js
+++ AccountDelete/account-delete.js
@@ -2,3 +2,7 @@ import { Client, Wallet } from 'xrpl'
 
 // Get wallet from seed
 const wallet = Wallet.fromSeed('TBC')
+
+// New ledger connection
+const client = new Client('TBC')
+await client.connect()
