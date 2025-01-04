cbb46fbdffcaafb510bf33d5d7cf4068417643e1
Jacob Wingate <jacob.wingate33@gmail.com>
2025-01-04 15:23:13 +0100

    add wallet from seed, and new ledger connection

--- Payment/payment.js
+++ Payment/payment.js
@@ -1,1 +1,17 @@
-const test = a + b
+import { Client, Wallet, xrpToDrops } from 'xrpl'
+
+// Get wallet from seed
+const wallet = Wallet.fromSeed('sEdSuFyxRq8qaxH57V859XULo38NXLr')
+
+// New ledger connection
+const client = new Client('wss://s.altnet.rippletest.net:51233')
+await client.connect()
+
+try {
+  // Create payment object
+  // Prepare transaction
+  // Sign the transaction
+  // Submit the transaction and wait for the result
+} catch (error) {
+} finally {
+}
