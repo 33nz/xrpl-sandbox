ff0f9daf3507156dfcaabacaf964657d51ed51b7
Jacob Wingate <jacob.wingate33@gmail.com>
2025-01-05 12:01:46 +0100

    add account delete code

--- AccountDelete/account-delete.js
+++ AccountDelete/account-delete.js
@@ -1,8 +1,32 @@
 import { Client, Wallet } from 'xrpl'
 
 // Get wallet from seed
-const wallet = Wallet.fromSeed('TBC')
+const wallet = Wallet.fromSeed('sEd7TBhUooHqhMfx2w2BnoLjLF3KS6Q')
 
 // New ledger connection
-const client = new Client('TBC')
+const client = new Client('wss://s.altnet.rippletest.net:51233')
 await client.connect()
+
+try {
+  // Create the account delete object
+
+  const deleteTx = {
+    Account: wallet.classicAddress, // rAddress we want to delete (ie this account)
+    TransactionType: 'AccountDelete', // Important to spell tis correctly, case sensitive
+    Destination: 'rNdN5RgiqxyUB4KsXAopGAubvdnnVtFVPr', // the account where any remaining balance will be sent
+  }
+
+  // Prepare the transaction
+  const prepared = await client.autofill(deleteTx) // Autofill to save time
+
+  // Sign the transaction
+  const signed = wallet.sign(prepared)
+
+  // Submit to the ledger
+  const result = await client.submitAndWait(signed.tx_blob)
+  console.log(result)
+} catch (error) {
+  console.log(error)
+} finally {
+  await client.disconnect()
+}
