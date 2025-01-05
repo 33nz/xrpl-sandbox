b7a5755a5030a2504a22c455f99f5dbaeeecaa77
Jacob Wingate <jacob.wingate33@gmail.com>
2025-01-05 10:56:18 +0100

    add payment code

--- Payment/payment.js
+++ Payment/payment.js
@@ -9,9 +9,24 @@ await client.connect()
 
 try {
   // Create payment object
+  const payment = {
+    Account: wallet.classicAddress, // rAddress of the sending account
+    TransactionType: 'Payment', // case important
+    Amount: xrpToDrops(1), // Important: Amount of XRP, converted to drops
+    Destination: 'rs2eb24PLdXjwQJnsE5phieHgSy1j4eJHe',
+  }
+
   // Prepare transaction
+  const prepared = await client.autofill(payment) // Autofill saves us some time
+
   // Sign the transaction
+  const signed = wallet.sign(prepared)
+
   // Submit the transaction and wait for the result
+  const result = await client.submitAndWait(signed.tx_blob)
+  console.log(result)
 } catch (error) {
+  console.log(error)
 } finally {
+  await client.disconnect()
 }
